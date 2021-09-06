e8d067b3d7e10ed7a6126f06af3345f31719d95f
manggong <jihwank1995@gmail.com>
2021-09-06 20:44:58 +0900

    add Column

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -25,12 +25,30 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("제목")
       ),
-      body: Container(
-        color: Colors.red,
-        width: 100,
-        height: 100,
-        padding: const EdgeInsets.all(8.0),
-        margin: const EdgeInsets.all(8.0)
+      body: Column(
+        children: <Widget>[
+          Container(
+            color: Colors.red,
+            width: 100,
+            height: 100,
+            padding: const EdgeInsets.all(8.0),
+            margin: const EdgeInsets.all(8.0)
+          ),
+          Container(
+            color: Colors.green,
+            width: 100,
+            height: 100,
+            padding: const EdgeInsets.all(8.0),
+            margin: const EdgeInsets.all(8.0)
+          ),
+          Container(
+            color: Colors.blue,
+            width: 100,
+            height: 100,
+            padding: const EdgeInsets.all(8.0),
+            margin: const EdgeInsets.all(8.0)
+          )
+        ]
       ) // 본문 들어갈 부분
     );
   }
