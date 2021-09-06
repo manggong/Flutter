a8fea74da177efb593b6eac3ec56ef61dfd45498
manggong <jihwank1995@gmail.com>
2021-09-06 20:31:05 +0900

    basic form

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -5,65 +5,27 @@ void main() {
   runApp(MyApp());
 }
 
-//시작 클래스, 메터리얼 디자인 앱 생성
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "Flutter App",
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'), // 표시할 화면의 인스턴스
+      home: MyHomePage()
     );
   }
 }
 
-// 시작 클래스가 실제로 표시하는 클래스, 카운터 앱 화면
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-// MyHomwPage 클래스의 상태를 나타내는 State 클래스
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("제목")
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+      body: Text("공부하는데 소현이가 노래 부른다!")
     );
   }
 }
